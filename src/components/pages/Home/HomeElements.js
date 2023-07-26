cc2474b4a23ca67272f86299a128bf1245ce7554
Solomon Vana <solomonvana18@gmail.com>
2023-07-26 08:29:15 -0500

    Made it so text was over main image and preped page for future additions

--- src/components/pages/Home/HomeElements.js
+++ src/components/pages/Home/HomeElements.js
@@ -14,15 +14,18 @@ padding: 5%;
 
 export const Img = styled.img`
 filter: brightness(.75);
-object-fit: scale-down;
 height: 700px;
 z-index: 0;
 background-image: url(${image});
 `;
 
 export const Div2 = styled.div`
+// Ratio 16:9
 filter: brightness(.75);
-// object-fit: scale-down;
 height: 700px;
+width: calc(( 700px / 9) * 16);
 background-image: url(${image});
+display: flex;
+justify-content: center;
+align-items: center;
 `;
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,29 +1,50 @@
 import React from 'react';
 import {Div, Main, Div2, Img} from "./HomeElements";
 import Banner from "../../../images/deep-forest-relaxation-2.jpg";
-import Carousel from "../../Carousel";
+// import Carousel from "../../Carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Home = () => {
 		return (
 			<Main >
 				{/* <Carousel /> */}
-				{/* <h1>My Projects</h1> */}
-				{/* <br/> */}
 				<Div>
-				{/* <Div style={{backgroundImage:`url(${Banner})`}}> */}
-					{/* <Div2> */}
-						{/* Welcome  */}
-					{/* <Img src={Banner}></Img> */}
-					{/* </Div2> */}
-					
+					<Div2 >
+						<p className='weave'>
+						Welcome 
+						</p>
+						
+					</Div2>
 				</Div>
-				
-				{/* <Box>
-welcome
-				</Box> */}
+				<div>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+					<br/>
+				</div>
 			</Main>
 		);
-		};
+	};
 
 export default Home;
+
+
+// const Home = () => {
+// 	return (
+// 		<Main >
+// 			<Div>
+// 				{/* <Img src={Banner}></Img> */}
+				
+// 			</Div>
+// 		</Main>
+// 	);
+// 	};
+
+// export default Home;
